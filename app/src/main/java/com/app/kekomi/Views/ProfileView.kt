f550bf06a7137c4be1e73bfc4a52048feff0f675
Camila Guillan <camiguillan@gmail.com>
2023-05-13 23:27:49 -0300

    alinie las checkboxes

--- app/src/main/java/com/app/kekomi/Views/ProfileView.kt
+++ app/src/main/java/com/app/kekomi/Views/ProfileView.kt
@@ -10,12 +10,20 @@ import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -67,7 +75,7 @@ fun PesoYAltura() {
             label = { Text("Height", fontSize = 18.sp) },
             modifier = Modifier.weight(1f).padding(8.dp).background(Color.White)
         )
-    } 
+    }
 }
 
 @Composable
@@ -126,23 +134,20 @@ fun CheckBoxes() {
                 modifier = Modifier
                     .padding(vertical = 8.dp)
                     .fillMaxWidth(),
-                verticalAlignment = Alignment.Top,
-                horizontalArrangement = Arrangement.SpaceEvenly
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Box(
-                    modifier = Modifier
-                        .background(Color.White)
-                        .clip(shape = RoundedCornerShape(4.dp))
-                        .size(18.dp)
-                ) {
-                    Checkbox(
-                        checked = checkedItems[index],
-                        onCheckedChange = { isChecked ->
+                Checkbox(
+                    checked = checkedItems[index],
+                    onCheckedChange = { isChecked ->
+                        if(items[index] != "Calories"){
                             checkedItems[index] = isChecked
-                        },
-                        //modifier = Modifier.padding(end = 8.dp)
-                    )
-                }
+                        }
+
+                    },
+                    //modifier = Modifier.padding(end = 8.dp)
+                )
+
                 Text(
                     text = item,
                     color = Color.Black,
@@ -150,27 +155,53 @@ fun CheckBoxes() {
                     modifier = Modifier.padding(start = 8.dp)
                 )
 
-                goal(item)
+                goal(item,  checkedItems[index])
             }
+
         }
     }
 }
 
 @Composable
-fun goal(item: String) {
+fun goal(item: String, isChecked: Boolean) {
     var inputValueG by remember { mutableStateOf("") }
 
     TextField(
         value = inputValueG,
         onValueChange = { newValue -> inputValueG = newValue },
-        label = { Text(item + " goal", fontSize = 15.sp, textAlign = TextAlign.Center) },
+        label = { Text( "Set goal", fontSize = 15.sp, textAlign = TextAlign.Center) },
         placeholder = { Text("") },
         modifier = Modifier
             .padding( 1.dp)
             .background(Color.White)
-            .width(150.dp)
-            .height(40.dp)
+            .width(100.dp),
+        keyboardOptions = KeyboardOptions.Default.copy(
+            keyboardType = KeyboardType.Number
+        ),
+        textStyle = TextStyle(textAlign = TextAlign.End),
+        enabled = isChecked,
+        readOnly = !isChecked,
+        visualTransformation = SuffixVisualTransformation(" g")
+
+
     )
+}
+
+
+class SuffixVisualTransformation(private val suffix: String) : VisualTransformation {
+    override fun filter(text: AnnotatedString): TransformedText {
+        val transformedText = text + AnnotatedString(suffix, SpanStyle(Color.Gray))
+
+        return TransformedText(transformedText, SuffixOffsetMapping(text.text))
+    }
+}
+
+class SuffixOffsetMapping(private val originalText: String) : OffsetMapping {
+    override fun originalToTransformed(offset: Int): Int = offset
+
+    override fun transformedToOriginal(offset: Int): Int {
+        return if (offset > originalText.length) originalText.length else offset
+    }
 }
 
 
