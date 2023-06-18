81bc1535fe93f74e9451d93fe8acf0744bc4cdd2
Carolina Blanco <cablanco@frba.utn.edu.ar>
2023-06-18 11:56:09 -0300

    investigando como hacer un widget

---
+++ app/src/main/java/com/app/kekomi/widget/KekoWidget.kt
@@ -0,0 +1,46 @@
+package com.app.kekomi.widget
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import androidx.glance.Button
+import androidx.glance.ButtonColors
+import androidx.glance.GlanceModifier
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.background
+import androidx.glance.layout.Alignment
+import androidx.glance.layout.Column
+import androidx.glance.layout.Row
+import androidx.glance.layout.fillMaxSize
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
+
+
+object KekoWidget: GlanceAppWidget() {
+
+    @Composable
+    override fun Content(){
+        Row(
+            modifier = GlanceModifier.fillMaxSize().background(Color.White),
+            horizontalAlignment = Alignment.Horizontal.CenterHorizontally,
+            verticalAlignment = Alignment.Vertical.CenterVertically
+        ){
+            Column() {
+                /*Button(text = "+     Add food",
+                    onClick = ,
+                    style = TextStyle(
+                        color= ColorProvider(color = Color.White),
+                        fontSize = 16.sp,
+                        textAlign = TextAlign.Center,
+                    ),
+                    modifier = GlanceModifier.fillMaxSize().background(color=Color(android.graphics.Color.parseColor("#008080")))
+                )*/
+            }
+            Column() {
+                
+            }
+        }
+    }
+}
